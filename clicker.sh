8702f02b9cb54e038a761ff193d34dd2a84d5233
Peter Simoncic <peter.simoncic@websupport.eu>
2021-11-30 18:03:32 +0100

    add possibility to button switching

--- clicker.sh
+++ clicker.sh
@@ -5,6 +5,7 @@ frequency=1
 primaryMouseButton=1
 secondaryMouseButton=3
 disableSecondary=
+switchButtons=
 buttonAction="click"
 
 function help() {
@@ -14,17 +15,19 @@ function help() {
   echo "Optional parameters (need to be added before other parameters):"
   echo "  f - frequency of clicking in seconds. Default '1'"
   echo "  m - hold button instead of click. Default 'false'"
-  echo "  d - don't hold secondary button (e.g. eat). Default 'true'"
+  echo "  d - disable holding of secondary button (e.g. eat). Default 'false'"
+  echo "  s - Switch buttons. Default 'false'"
   echo "  h - show help"
 
   exit
 }
 
-while getopts ":f:dmh" opt; do
+while getopts ":f:dmsh" opt; do
   case "${opt}" in
   f) frequency="${OPTARG}" ;;
   d) disableSecondary=true ;;
   m) buttonAction="mousedown" ;;
+  s) switchButtons=true ;;
   h) help ;;
   *)
     echo "Invalid Option: -$OPTARG" 1>&2
@@ -36,6 +39,13 @@ shift $((OPTIND - 1))
 
 
 windowId=$(xdotool search --classname "$windowClassName")
+
+if [[ "$switchButtons" ]]; then
+  tmpBtn="$primaryMouseButton"
+  primaryMouseButton="$secondaryMouseButton"
+  secondaryMouseButton="$tmpBtn"
+fi
+
 
 if [[ -z "$disableSecondary" ]]; then
   xdotool mousedown --window "$windowId" "$secondaryMouseButton" &
